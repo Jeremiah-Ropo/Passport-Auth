32e0004cee39a7269ed220d95c37d62b70a95be7
Patrick Jeremiah <patrickjerry59@yahoo.com>
2022-03-02 17:03:04 +0100

    in progress

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+
+
 app.listen(3000,function () { 
     console.log('Server started on port 3000')
 });
